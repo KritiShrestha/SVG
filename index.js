24ed085499fee5b94d7c6ac79e90ad7afb6fbde5
Kriti Shrestha <Kriti@Sarojs-MacBook-Pro.local>
2023-04-04 23:21:53 -0400

    Updated and cleaned the code

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
 //Importing inquirer to ask the question to user
-const inquirer = require ('inquirer');
+const inquirer = require('inquirer');
 //fs to write the file
 const fs = require('fs');
+// Importing generateMarkdown.js file
+const generateSVG = require('./lib/generateContent');
 
 //An array of questions to ask the user
 const questions = [
     {
-    type: 'input',
-    message: "Please enter the characters to build the text.",
-    name: 'character'
+        type: 'input',
+        message: "Please enter the characters to build the text.",
+        name: 'character'
     },
 
     {
         type: 'input',
-    message: "Please enter the color keyword or a hexadecimal number to add color to text.",
-    name: 'textcolor'
+        message: "Please enter the color keyword or a hexadecimal number to add color to text.",
+        name: 'textcolor'
     },
 
     {
-    type: 'list',
-    message: "Please choose from the following option to choose the shape",
-    choices: ["Circle", "Triangle", "square"],
-    name: 'shape'
+        type: 'list',
+        message: "Please choose from the following option to choose the shape",
+        choices: ["Circle", "Triangle", "Square"],
+        name: 'shape'
     },
 
-    {type: 'input',
-    message: "Please enter the color keyword or a hexadecimal number to add color to shape.",
-    name: 'shapecolor'
+    {
+        type: 'input',
+        message: "Please enter the color keyword or a hexadecimal number to add color to shape.",
+        name: 'shapecolor'
+    }
+]
+
+//  Creating a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        const svgContent = generateSVG(data)
+        //writes the svg to logo.svg file
+        fs.writeFile('logo.svg', svgContent, (err) => {
+            err ? console.log(err) : console.log("Successfully generated SVG file")
+        });
+    });
+
 }
 
-]
-//after user has answered the question, the name property of data object is assigned to a variable
-function init (){
- inquirer.prompt(questions).then((data)=>{
-        //const filename = data.character
-        //writing a file with two params i.e filename and content
-        fs.writeFile('logo.svg', 'logo',(err)=>{
-        err ? console.log(err) : console.log("Successfully generated SVG file")
-        });
-        });
-}
+// Function call to initialize app
 init();
